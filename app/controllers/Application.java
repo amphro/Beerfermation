ef014eb205f7e8da15f73b498c44429839fd943d
Amphro <amphro@Amphro-home.(none)>
2013-03-30 19:06:57 -0700

    Adding style, home page, and hops page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,23 @@ import models.HopDefinition;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.hops;
+import views.html.login;
 
 public class Application extends Controller {
   
     public static Result index() {
         return ok(index.render(HopDefinition.find.all()));
+    }
+    
+    public static Result hops() {
+    	return ok(hops.render(HopDefinition.find.all()));
+    }
+    
+    public static Result login() {
+        return ok(
+            login.render()
+        );
     }
   
 }
